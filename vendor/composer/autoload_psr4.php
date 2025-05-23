84f688746a63586c582417d552a1ddd3da05f1e7
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-05-24 00:20:08 +0300

    🚀 Added: Database Query API. (#13)
    * 🚀 Added: Database Query API.
    
    * 🔥 Updated: Readme example code.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Xenioushk\\BwlPluginApi\\' => array($baseDir . '/src'),
+    'PHPCSStandards\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
 );
